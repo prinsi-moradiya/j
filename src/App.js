dfee64ba4b6a98d8a6d22c4030d9837347343c8b
prinsi-moradiya <moradiyaprinsi@gmail.com>
2025-04-17 11:24:48 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>Hey, Hello! 👋</h1>
+      <p>Welcome to your first React app!</p>
+    </div>
+  );
+}
+
+export default App;
